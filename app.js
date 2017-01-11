97f85b91ac144c3665b166e21467eedbd2505672
sureshpriyanka <sureshpriyanka@users.noreply.github.com>
2017-01-11 14:58:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var sum=0,sum1=0;
+var j=0;
+
+
+for(var i=0;i<1001;i++)
+sum+=i;
+
+console.log("SUM of 1-1000 : "+sum);
+
+while(j<1001)
+{
+ sum1+=j;
+ j++;
+
+ }
+
+console.log("SUM of 1-1000 : "+sum1);
